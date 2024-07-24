00a865b8e7b2963d5ae16003a8fd037d713de53f
junjun932 <162272537+junjun932@users.noreply.github.com>
2024-07-24 12:02:20 +0900

    Add files via upload
    실습제출 .

---
+++ '걷기반_실습'/240724/1.js
@@ -0,0 +1,5 @@
+
+const greeting = "Hello";
+const name1 = "World";
+
+console.log(greeting + " " +name1);
---
+++ '걷기반_실습'/240724/4.js
@@ -0,0 +1,12 @@
+// 문제 2번의 사자 객체를 가져옵니다.
+// const lion = { ... }
+const lion = { name:"심바" , age:3 , mainCharacter:true , daeSa:"하쿠나마타타"};
+// 나이를 추가하는 함수입니다.
+function incrementAge(lion) {
+    lion.age += 1;
+
+    return lion;
+  }
+  
+  // 함수 실행 이후 사자의 현재 나이를 출력합니다.
+  console.log(incrementAge(lion));
