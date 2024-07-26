e5faa3dd1ac6a542555022347bf0a45b89ec93a9
junjun932 <162272537+junjun932@users.noreply.github.com>
2024-07-26 12:46:24 +0900

    Add files via upload
    silseub

---
+++ 240726/2.js
@@ -0,0 +1,11 @@
+function checkAge(age) {
+   if (age >= 19){
+    return "안녕하세요";
+   }else {}
+   (age<=19)
+    return "나가세요";
+   }
+    
+    // 함수 실행
+  console.log(checkAge (38));
+  console.log(checkAge(7)); 
---
+++ 240726/3.js
@@ -0,0 +1,8 @@
+function repeatWord(str, n) {
+    for (let i=0 ; i<5; i++)
+        console.log(str);
+  }
+  
+  // 함수 실행
+
+  repeatWord("hello",5);
